c04cd5077853ba4fc504ec5699b9401d853bf9e7
prashubisht15 <prashantsinghbisht710@gmail.com>
2020-08-12 12:47:52 +0530

    finished dp matrix

--- matrix.cpp
+++ matrix.cpp
@@ -3,5 +3,27 @@ using namespace std;
 
 int main()
 {
+    int m,n;
+    cin>>m>>n;
 
+    int arr[m][n];
+
+    for(int i=0; i<m; i++)
+    {
+        arr[i][0]=1;
+    }
+    for(int i=0; i<n; i++)
+    {
+        arr[0][i]=1;
+    }
+
+    for(int i=1; i<m; i++)
+    {
+        for(int j=1; j<n; j++)
+        {
+            arr[i][j]=arr[i-1][j] + arr[i][j-1];
+        }
+    }
+
+    cout<<arr[m-1][n-1];
 }
